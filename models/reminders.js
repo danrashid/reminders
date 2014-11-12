161fb10d75c15cd0c373e2f1627212f207402a0b
Dan Rashid <drashid@signal.co>
2014-11-12 05:53:21 -0600

    Adding create Reminder
    Here we setup the MongoDB driver and the model, view, and controller (route) for creating a Reminder.
    
    Try submitting some and watch your Node log. Then try this in your mongo client:
    
    db.reminders.find();

---
+++ models/reminders.js
@@ -0,0 +1,27 @@
+/*
+Sources:
+  • https://www.npmjs.org/package/mongodb
+  • http://mongodb.github.io/node-mongodb-native/1.4/markdown-docs/queries.html
+*/
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/myproject';
+
+exports.create = function (req, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected correctly to server');
+
+    db.collection('reminders').insert({
+      name: req.body.name,
+      date: req.body.date
+    }, function(err, result) {
+      assert.equal(err, null);
+      console.log('Inserted ' + JSON.stringify(req.body));
+      db.close();
+      console.log('Connection closed');
+      callback(result);
+    });
+  });
+};
--- routes/reminders.js
+++ routes/reminders.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../models/reminders');
 
-router.get('/', function(req, res) {
-  res.render('reminders', { title: 'Reminders!' });
-});
+router
+  .get('/', function(req, res) {
+    res.render('reminders', { title: 'Reminders!' });
+  })
+  .post('/', function(req, res) {
+    model.create(req, function () {
+      // Always redirect after POSTing! (unless it's AJAXy)
+      res.redirect('/reminders');
+    });
+  });
 
 module.exports = router;
