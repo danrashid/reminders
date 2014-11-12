438c9064748271825a51bc00893a23367d993c20
Dan Rashid <drashid@signal.co>
2014-11-11 21:37:55 -0600

    Adding first route and view
    Visiting / should really present the user with a login form, but authentication is too much to cover right now.
    
    Let's instead pretend the user has logged in and been redirected to their own /reminders view.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // Login form should really, absolutely be here
+  res.redirect('/reminders');
 });
 
 module.exports = router;
--- routes/reminders.js
+++ routes/reminders.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('reminders', { title: 'Reminders!' });
 });
 
 module.exports = router;
